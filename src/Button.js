c31ce16c625f21539e642822c9a080991d34d63e
peppersora <110614387+peppersora@users.noreply.github.com>
2023-01-02 16:14:07 +0900

    prop types

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,10 @@
 // component = function
 // component는 jsx를 return하는 함수
-function Btn({potato, changeValue}){
+
+function Btn(text,fontSize){
    
-    return <button 
-    onclick={changeValue}
+    return (
+    <button 
     style={{
         // button에 style속성부여
         backgroundColor:"orange",
@@ -11,33 +12,26 @@ function Btn({potato, changeValue}){
         padding: "10px 20px",
         border: 0 ,
         borderRadius:10,
-
+        fontSize,
     }}
-    >{potato}</button>;
+    >
+
+    </button>
+);
 }
 
 // root div 가 됨
 function App () {
-    // onclick 이벤트를 이용할것이라 state사용
-    // rerender 할때 continue btn을 우리가 통제할수있다.
-    const MemorizedBtn = React.memo(Btn);
-    const [value, setValue] = React.useState("Save changes");
-    const changeValue = () => setValue("Revert Changes")
+   
     return(
         <div>
-            {/* 여기서 onclick은 실제 이벤트리스너가 아니고
-            Btn함수에 쓰이는 props이다.
-         */}
-            <MemorizedBtn text={value} changeValue={changeValue}/>
-            
-            <MemorizedBtn text="Continue" />
+       <Btn text="Save Changes" fontSize={18}></Btn>
+       <Btn text="Save Changes" fontSize={18}></Btn>
     </div>
     );
 }
 const root = document.getElementById("root");
 ReactDOM.render(<App/>,root);
-
-
 
 
 
