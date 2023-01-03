c7bde681ff246ac2dbf30c0df769ea73f78d2546
peppersora <110614387+peppersora@users.noreply.github.com>
2023-01-03 12:27:36 +0900

    effect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-   <div><h1>Welcome back!!!</h1></div>
+function App(){
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev+1);
+  const onchange = (event) => {
+    setKeyword(event.target.value);
+  }
+  console.log("I run all the time");
+  
+  useEffect(() =>{
+    console.log("CALL THE API...");
+  },[]);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length >5) {
+      console.log("SEARCH FOR", keyword)
+    }
+  },[keyword]);
+
+   return(
+    <div>
+      <input 
+      value={keyword}
+      onchange={onchange} 
+      type="text" 
+      placeholder="Search here"
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
+    </div>
   );
 }
-
 export default App;
