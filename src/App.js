9e0adfa20fcf3d29252c6583566fd220867a4b94
peppersora <110614387+peppersora@users.noreply.github.com>
2023-01-04 12:34:00 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { useEffect, useState } from "react";
 
 //무엇을 숨기고 보여줄 것인가?
 function Hello(){
-  useEffect(() => {
-    console.log("created :)");
-    // 우리가 할것은 component가 destory될때 실행될 
-    // function만들기 => clean up function
-    // 아래 코드한줄을 이용해 언제 만들어졌고 언제 destroy됐는지 알수있다
-    return () => console.log("destroyed :(")
-  },[]);
 
+  function byFn(){
+    console.log("bye :(");
+  }
+
+  function hiFn(){
+    console.log("created :)");
+    return byFn;
+  }
+
+  useEffect( hiFn,[]);
+   
   return(
     <h1>Hello</h1>
   );
